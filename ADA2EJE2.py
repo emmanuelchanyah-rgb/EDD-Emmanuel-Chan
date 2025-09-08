ab09c8c11ccaffb03f5814bdfc71db5c1378aad8
emmanuelchanyah-rgb <emmanuelchanyah@gmail.com>
2025-09-07 23:31:00 -0600

    Add files via upload

---
+++ ADA2EJE2.py
@@ -0,0 +1,16 @@
+#Emmanuel andres chan
+frutas = []
+
+frutas.append("Mango")
+frutas.append("Manzana")
+frutas.append("Banana")
+frutas.append("Uvas")
+
+print("Frutas iniciales:", frutas)
+
+
+frutas.pop(0)  
+frutas.pop(1)  
+frutas.append("Sandía")
+
+print("Frutas finales:", frutas)
